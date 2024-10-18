c4ed52f8935d743c32ef04d1a121878e89fb4bab
Mini-Valeria <anavaleriap257@gmail.com>
2024-10-17 20:04:01 -0600

    Frontend actual

--- src/Vnet_frontend/src/App.jsx
+++ src/Vnet_frontend/src/App.jsx
@@ -1,12 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-
 
 export default function App() {
-  
-
   return(
     <main>
       <div className='p'>
@@ -14,17 +9,17 @@ export default function App() {
       <header className='header'>
         
       <div className='logo'>
-      <img className='logoimg' src='/src/imgs/logoimg.png'></img>
-      
-      
+      <img className='logoimg' src='/src/imgs/png.png'></img>
       </div>
 
           <nav>
+              <a href="javascript: history.go(-1)" className='navlink'>◀</a>
               <a href='/sos' className='navlink'>S.O.S</a>
-              <a href='https://identity.ic0.app/' className='navlink '>login</a>
+              <a href='https://identity.ic0.app/' className='navlink'>Login</a>
               <a href='#' className='navlink '>About</a>
               <a href='#' className='navlink '>Account</a>
           </nav>
+
         </header>
         <hr className='hr'></hr>
         <div className='contenedorpadre'>
@@ -34,23 +29,58 @@ export default function App() {
           <h1>Vnet</h1>
         <p >Ingresa tu destino</p>
         <Form>
-        <Form.Control type='email' placeholder='Ubicacion' />
+        <Form.Control placeholder='Ubicacion' />
     </Form>
     <br></br>
-          <a href='/Home' className='botin'></a>
+          <a href='/Home' type='Submit' className='botin'>🔍</a>
         </div>
         </div>
         
         <hr className='hr'></hr>
+        <div className='contlabel2'>
+        <h3 className='h3'>Recientes</h3>
+        <p className='p1'>Feedbacks</p>
+        <hr className='hr' />
+        
+        <div className='contlabel2'>
+          <div className='contBox'>
+            <div className='card' src='./imgs/background.jpg'>
+              <div className='textos'>
+              <h5>Ubicación 1</h5>
+              <p>Descripción</p>
+              </div>
+          </div>
+
+          <div className='card'>
+              <div className='textos'>
+              <h5>Ubicación 2</h5>
+              <p>Descripción</p>
+              </div>
+          </div>
+
+          <div className='card'>
+              <div className='textos'>
+              <h5>Ubicación 3</h5>
+              <p>Descripción</p>
+              </div>
+          </div>
+
+          <div className='card'>
+              <div className='textos'>
+              <h5>Ubicación 4</h5>
+              <p>Descripción</p>
+              </div>
+          </div>
         </div>
         
+        </div>
+        <br></br>
+          </div>
+        </div>
    </div>
 
     </main>
   )
-  
-
-
 }
 
   
--- src/Vnet_frontend/src/Home.jsx
+++ src/Vnet_frontend/src/Home.jsx
@@ -8,14 +8,15 @@ export default function Home () {
             <header className='header'>
         
         <div className='logo'>
-        <img className='logoimg' src='/src/imgs/logoimg.png'></img>
+        <img className='logoimg' src='/src/imgs/png.png'></img>
         
         
         </div>
   
             <nav>
+                <a href="javascript: history.go(-1)" className='navlink'>◀</a>
                 <a href='#' className='navlink'>S.O.S</a>
-                <a href='/login' className='navlink '>login</a>
+                <a href='/login' className='navlink '>Login</a>
                 <a href='#' className='navlink '>About</a>
                 <a href='#' className='navlink '>Account</a>
             </nav>
