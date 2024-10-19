995b1633ea1e905e94563a9fc2be4567a5c43269
Mini-Valeria <anavaleriap257@gmail.com>
2024-10-19 11:00:13 -0600

    Combinando final con main

--- src/Vnet_frontend/src/App.jsx
+++ src/Vnet_frontend/src/App.jsx
@@ -1,5 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from 'react-bootstrap/Form';
 import { useEffect, useState } from 'react';
 import { useRestActor } from "@bundly/ares-react";
 
@@ -13,45 +11,9 @@ function App() {
   
 
   return(
-    <main>
-      <div className='p'>
-      <meta name='viewport' content='widht=device-widht, initial-scale=1.0'></meta>
-      <header className='header'>
-        
-      <div className='logo'>
-      <img className='logoimg' src='./src/assets/images/logoimg.png'></img>
-      
-      
-      </div>
-
-          <nav>
-              <a href='/sos' className='navlink'>S.O.S</a>
-              <a href='https://identity.ic0.app/' className='navlink '>login</a>
-              <a href='#' className='navlink '>About</a>
-              <a href='#' className='navlink '>Account</a>
-          </nav>
-        </header>
-        <hr className='hr'></hr>
-        <div className='contenedorpadre'>
-        <div className='conthijo'>
-        <div className='contlabel'>
-        
-          <h1>Vnet</h1>
-        <p >Ingresa tu destino</p>
-        <Form>
-        <Form.Control type='email' placeholder='Ubicacion' />
-    </Form>
-    <br></br>
-          <a href='/Home' className='botin'></a>
-        </div>
-        </div>
-        
-        <hr className='hr'></hr>
-        </div>
-        
-   </div>
-
-    </main>
+    <>
+    <p>Working</p>
+    </>
   )
 }
 
